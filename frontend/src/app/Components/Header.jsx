ff66cb834ad59895ddb4ada7e47f000121d19658
AKAZJAYA <savindu096@gmail.com>
2025-08-18 14:30:27 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/src/app/Components/Header.jsx
+++ frontend/src/app/Components/Header.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center py-[1.5em] px-[15em] bg-white text-black text-2xl">
-      <div>ZorsCode - Academy</div>
-      <div className="flex justify-between w-[8em] text-2xl">
+    <div className="flex justify-between items-center py-[1.5em] px-[10em] bg-white text-[#FF7D29] text-[2em]">
+      <div>ZorsCode Academy</div>
+      <div className="flex justify-between w-[8em] text-[1em]">
         <div>Login</div>
         <div>Signup</div>
       </div>
