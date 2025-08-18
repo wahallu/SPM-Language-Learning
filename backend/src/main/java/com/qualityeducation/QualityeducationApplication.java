c4ff292d1f85f6db9838a0ebe261656de629412f
Nisal Fonseka <nisalfonseka@gmail.com>
2025-08-18 11:51:28 +0530

    Add global styles and layout for Next.js application
    - Created globals.css for global styles including dark mode support.
    - Implemented layout.js to set up the root layout with custom fonts.
    - Developed page.js to serve as the main page with a structured layout and links to documentation and deployment.

---
+++ backend/src/main/java/com/qualityeducation/QualityeducationApplication.java
@@ -0,0 +1,13 @@
+package com.qualityeducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityeducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityeducationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/qualityeducation/QualityeducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qualityeducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityeducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
