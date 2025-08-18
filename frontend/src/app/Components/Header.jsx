c19450df69dd0f7b38b5310bd61d483e6da3aace
AKAZJAYA <savindu096@gmail.com>
2025-08-18 13:11:02 +0530

    Implement Header component with layout and integrate into Home page

--- frontend/src/app/Components/Header.jsx
+++ frontend/src/app/Components/Header.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+'use client'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className="flex justify-between items-center py-[1.5em] px-[20em] bg-white text-black">
+      <div>Logo</div>
+      <div className="flex justify-between w-[8em]">
+        <div>Login</div>
+        <div>Signup</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
