2d35ba9e6f73bc9941da77607b3e0a364a513fae
Nisal Fonseka <nisalfonseka@gmail.com>
2025-08-18 13:10:22 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Header from './Components/Header'
+import Hero from './Home/Hero'
+import Footer from './Components/Footer'
 
 const Home = () => {
   return (
     <div>
+      
+      <Hero />
+      
       
     </div>
   )
