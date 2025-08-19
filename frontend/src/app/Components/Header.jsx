fdd0dfac3e833d2a02dd7190b6b8dc16c90305bb
Nisal Fonseka <nisalfonseka@gmail.com>
2025-08-19 14:09:24 +0530

    Implement code changes to enhance functionality and improve performance

--- frontend/src/app/Components/Header.jsx
+++ frontend/src/app/Components/Header.jsx
@@ -1,10 +1,19 @@
 'use client'
 import React from "react";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center py-[1.5em] px-[10em] bg-[#fffbf9] text-[#FF7D29] text-[2em]">
-      <div>ZorsCode Academy</div>
+    <div className="flex justify-between items-center py-[1.5em] px-[10em] bg-[#FF7D29]/5  text-[#FF7D29] text-[2em]">
+      <div>
+        <Image
+          src="/ZAlogo.png"
+          alt="Logo"
+          width={100}
+          height={40}
+          unoptimized={true}
+        />
+      </div>
       <div className="flex justify-between w-[8em] text-[1em]">
         <div>Login</div>
         <div>Signup</div>
